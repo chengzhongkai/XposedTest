bec11b62f0a4ddc0f6193faebdc315e78cfeceec
cheng <zhongkai.cheng@gmail.com>
2016-05-25 12:47:26 +0900

    add listview

--- app/build/generated/source/r/debug/com/me/xposed/R.java
+++ app/build/generated/source/r/debug/com/me/xposed/R.java
@@ -18,15 +18,20 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int btOpewn=0x7f070006;
         public static final int card_list=0x7f070004;
+        public static final int cnt=0x7f070007;
         public static final int from=0x7f070000;
+        public static final int listView=0x7f070005;
         public static final int mailText=0x7f070002;
+        public static final int point=0x7f070008;
         public static final int send=0x7f070003;
         public static final int to=0x7f070001;
     }
     public static final class layout {
         public static final int activity_mail=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int id_row=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f060000;
